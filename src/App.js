ad55bd8c29e6e84a26926bdeebe9f777881b7adf
anshudevil07 <himanshupalariya13@gmail.com>
2023-01-29 12:07:13 +0530

    Submit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+function App(props){
+    return (
+     <>
+     <div className='cards'>
+       <div className='card'>
+         <img src={props.imgscr} alt="mypic" className='cards_image'/>
+           <div className='card_info'>
+             <span className='card_category'>{props.title}</span>
+             <h3 className='card_title'>{props.sname}</h3>
+             <a href={props.source} target='blank'>
+               <button className="btns">S</button>
+               <button className="btns">M</button>
+               <button className="btns">L</button>
+             </a>
+             <div className='botto'>
+              <h4>{props.con}</h4>
+              <p id="bot">{props.tag}</p>
+             </div>
+           </div>
+       </div>
+     </div>
+    </>
+   
+   );
+    }
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,65 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+ root.render(
+ <>
+
+
+
+<App
+  
+    imgscr="https://i.pinimg.com/originals/27/13/9d/27139dd4dab69b26d601514d70c04fd1.jpg"
+    title="White Traditional"
+    source="#"
+    tag="#MonHpY"
+    con="Monday Happy"
+ />
+
+<App
+   
+  
+    imgscr="https://m.media-amazon.com/images/I/61sSRRE0eoL._UL1024_.jpg"
+    title="sleve denim Jacket"
+    source="#"
+    tag="#SRPSPYDY"
+    con="payday surprise"
+    
+ />
+
+<App
+   
+  
+    imgscr="https://m.media-amazon.com/images/I/81zKavPkQZL._UL1500_.jpg"
+    title="Hush Pupples"
+    source="#"
+    tag="#HPYFRST"
+    con="first"
+ />
+
+
+
+
+
+<App
+   
+  
+    imgscr="https://cdn.shopify.com/s/files/1/0361/0391/3603/products/IMG_20220430_155544_261.webp?v=1651314640"
+    title="Athens Skirt"
+    source="#"  
+    tag="#VEGANLOVE"
+    con="vegetarian food"
+ />
+ {/* {Sdata.map(ncard)}; //chal ni ra hai */}
+
+ 
+</>
+ )
+
+
+
